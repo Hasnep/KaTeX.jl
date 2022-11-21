d04eda31712b364ef27818f34e046b5507d8548a
Hannes <h@nnes.dev>
2022-11-21 19:28:51 +0000

    Fix whitespace

--- src/KaTeX.jl
+++ src/KaTeX.jl
@@ -9,10 +9,10 @@ __init__() = run(`$(NodeJS.npm_cmd()) install katex`)
 function render_katex(s)
     render_script = """
     const katex = require('katex');
-    const result = katex.renderToString(`$s`, {throwOnError:false});
+    const result = katex.renderToString(`$s`, {throwOnError: true});
     console.log(result);
     """
-   return read(`$(NodeJS.nodejs_cmd()) --eval "$render_script"`, String)
+    return read(`$(NodeJS.nodejs_cmd()) --eval "$render_script"`, String)
 end
 
 end # module
